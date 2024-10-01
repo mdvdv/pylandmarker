d5c21929a2183a5ac652b2e8c4606fac59332465
a.medvedev <medvedev.workspace@yandex.ru>
2024-10-01 13:53:20 +0300

    Add detection models imports

---
+++ landmarker/models/__init__.py
@@ -0,0 +1,13 @@
+from landmarker.models.base_detector import BaseDetector
+from landmarker.models.face_detector import FaceDetector
+from landmarker.models.facemesh_detector import FaceMeshDetector
+from landmarker.models.palm_detector import PalmDetector
+from landmarker.models.pose_detector import PoseDetector
+
+__all__ = [
+    "BaseDetector",
+    "FaceDetector",
+    "FaceMeshDetector",
+    "PalmDetector",
+    "PoseDetector",
+]
